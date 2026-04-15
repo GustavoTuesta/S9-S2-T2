ef6fe3cdf2923b845922f47261437ec0afae0217
Gustavo Tuesta <tuesta2006@gmail.com>
2026-04-15 16:13:06 -0500

    linea horizontal

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function agregarTarea() {
     const lista = document.getElementById('lista');    
     const tarea = document.getElementById('tarea');
     lista.innerHTML += `<li>${tarea.value}</li>`;
+    lista.innerHTML += `<hr>`
 }
 
 li.onclick=function(){
